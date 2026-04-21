0b6e07aabff4f3c2135b357152ee253ec54e9aa1
Newton Adhikari <newtonadk@gmail.com>
2026-04-21 09:58:35 +0545

    created method for temperature scaling

---
+++ scripts/eval/compute_temperature_scaling.py
@@ -0,0 +1,11 @@
+import argparse
+from uncertainty_nav.models import PolicyNetwork, DeepEnsemble
+from uncertainty_nav.mc_dropout import MCDropoutPolicy
+from uncertainty_nav.nav_env import PartialObsNavEnv, ENV_A, ENV_B, ENV_C, ENV_D
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+if __name__ == "__main__":
+    main()
